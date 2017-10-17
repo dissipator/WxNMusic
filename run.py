ed580094747ea32d8c6ad26afbcc168b4d0f93f8
dissipator <dissipator_520@qq.com>
2017-10-17 22:29:36 +0800

    first commit

---
+++ run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+'''
+网易云音乐 Entry
+'''
+
+
+
+from WxNeteaseMusic import WxNeteaseMusic
+import itchat
+from itchat.content import *
+wnm = WxNeteaseMusic()
+
+@itchat.msg_register(itchat.content.TEXT)
+def mp3_player(msg):
+    text = msg['Text']
+    res = wnm.msg_handler(text)
+    # itchat.send(text,"filehelper")
+    itchat.send(res, 'filehelper')
+    # itchat.send(res)  
+    return res
+
+itchat.auto_login(hotReload=True,enableCmdQR=False)
+itchat.run(debug=False)
